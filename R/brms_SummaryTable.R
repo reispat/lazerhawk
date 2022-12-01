29ad844c8edccc1f86b359c805b37bc4a00a7b6d
Patrick Reisinger <patrick.reisinger@sbg.ac.at>
2022-12-01 14:46:30 +0100

    Update brms_SummaryTable.R

--- R/brms_SummaryTable.R
+++ R/brms_SummaryTable.R
@@ -90,7 +90,7 @@ brms_SummaryTable <- function(model,
     pp = hypetests$hypothesis$Post.Prob                  
     partables_formatted$pvals = pp*100
     partables_formatted$pvals[is.infinite(ER)] = 100
-    if (is.infinite(ER)) {              
+    if (is.infinite(ER[2])) {              
       partables_formatted$pvals = do.call(format, list(x=round(partables_formatted$pvals, 0), formatOptions[[1]]))
      } else {
       partables_formatted$pvals = do.call(format, list(x=round(partables_formatted$pvals, round), formatOptions[[1]]))
